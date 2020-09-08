946a5f2861ec009b0092753c24054d6edb969210
Nikush Dalia <nikushdalia@gmail.com>
2020-09-08 11:40:38 -0400

    TC-4686: fix blackduck vulnerabilities (#7)
    * TC-4686: fix 4 vulnerabilities
    
    * TC-4686: update version number
    
    * TC-4686: remove lodash

--- Gruntfile.js
+++ Gruntfile.js
@@ -222,7 +222,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
